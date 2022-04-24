e2f1bfd86563b2b7c585ef76beb07af91cdb3380
Christian Alberto Tamayo Robayo <giby098@hotmail.com>
2022-04-24 14:27:33 -0500

    Using sub tests

--- calculadora/calculator_test.go
+++ calculadora/calculator_test.go
@@ -4,6 +4,7 @@ import "testing"
 
 func TestDiscountCalculator(t *testing.T) {
 	type testCase struct {
+		name                  string
 		minimumPurchaseAmount int
 		discount              int
 		purchaseAmount        int
@@ -12,37 +13,43 @@ func TestDiscountCalculator(t *testing.T) {
 
 	testCases := []testCase{
 		{
+			name:                  "Should apply 20",
 			minimumPurchaseAmount: 100,
 			discount:              20,
 			purchaseAmount:        150,
 			expectedAmount:        130,
 		},
 		{
+			name:                  "Should apply 40",
 			minimumPurchaseAmount: 100,
 			discount:              20,
 			purchaseAmount:        200,
 			expectedAmount:        160,
 		},
 		{
+			name:                  "Should apply 60",
 			minimumPurchaseAmount: 100,
 			discount:              20,
 			purchaseAmount:        350,
 			expectedAmount:        290,
 		},
 		{
+			name:                  "Should not apply",
 			minimumPurchaseAmount: 100,
 			discount:              20,
-			purchaseAmount:        50,
+			purchaseAmount:        10,
 			expectedAmount:        50,
 		},
 	}
 
 	for _, tc := range testCases {
-		calculator := NewDiscountCalculator(tc.minimumPurchaseAmount, tc.discount)
-		amount := calculator.Calculate(tc.purchaseAmount)
+		t.Run(tc.name, func(t *testing.T) {
+			calculator := NewDiscountCalculator(tc.minimumPurchaseAmount, tc.discount)
+			amount := calculator.Calculate(tc.purchaseAmount)
 
-		if amount != tc.expectedAmount {
-			t.Errorf("Expected %v, got %v", tc.expectedAmount, amount) // Error = Log + Fail
-		}
+			if amount != tc.expectedAmount {
+				t.Errorf("Expected %v, got %v", tc.expectedAmount, amount) // Error = Log + Fail
+			}
+		})
 	}
 }
